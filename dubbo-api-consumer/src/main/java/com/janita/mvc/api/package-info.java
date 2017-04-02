9b322dfa759fb4c49fcb95bc2108c8efbda499ed
Janita <zhuchenjian@huizhi-tech.com>
2017-04-02 14:55:46 +0800

    provider-api

--- dubbo-api-consumer/src/main/java/com/janita/mvc/api/package-info.java
+++
@@ -1,5 +0,0 @@
-
-/**
- * Created by Janita on 2017-04-02 13:50
- */
-package com.janita.mvc.api;
--- dubbo-user-provider/src/main/java/com/janita/mvc/user/dao/UserDao.java
+++ dubbo-user-provider/src/main/java/com/janita/mvc/user/dao/UserDao.java
@@ -1,7 +1,10 @@
 package com.janita.mvc.user.dao;
 
+import org.springframework.stereotype.Repository;
+
 /**
  * Created by Janita on 2017-04-02 14:19
  */
+@Repository
 public class UserDao {
 }
